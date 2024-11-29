cce99250be527e77bfdd03bdb792f02d12ad7549
hongduck710 <hongik710@gmail.com>
2024-11-29 22:49:12 +0900

    깃허브 파인더 4 Footer Component부터 다시 복습

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,27 +4,29 @@ import PropTypes from "prop-types";
 
 function Navbar({title}){
     return (
-        <nav className="navbar mb-12 shadow-lg
-        bg-neutral text-neutral-content">
+        <nav className="navbar mb-12 shadow-lg bg-neutral
+        text-neutral-content">
             <div className="container mx-auto">
                 <div className="flex-none px-2 mx-2">
-                    <FaGithub  className="inline pr-2 text-3xl"/>
-                    <Link to="/" className="text-lg font-bold
-                    aling-middle">{title}</Link>
+                    <FaGithub className="inline pr-2 text-3xl" />
+                    <Link to="/" className="text-lg font-bold align-middle">
+                        {title}
+                    </Link>
                 </div>
-            </div>
-            <div className="flex-1 px-2 mx-2">
-                <div className="flex justify-end">
-                    <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
-                        Home
-                    </Link>
-                    <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
-                        About
-                    </Link>
+
+                <div className="flex-1 px-2 mx-2">
+                    <div className="flex justify-end">
+                        <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+                            Home
+                        </Link>
+                        <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+                            About
+                        </Link>
+                    </div>
                 </div>
             </div>
         </nav>
-    );
+    )
 }
 
 Navbar.defaultProps = {
@@ -34,4 +36,5 @@ Navbar.defaultProps = {
 Navbar.propTypes = {
     title: PropTypes.string
 }
+
 export default Navbar;
