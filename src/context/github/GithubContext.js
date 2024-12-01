a09a0678d833d5d4111e246993ef47e56ee6c426
hongduck710 <hongik710@gmail.com>
2024-12-01 17:12:58 +0900

    App.js에 GithubProvider컴포넌트로 감싸는 부분까지 에러없이 완료

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -5,7 +5,7 @@ const GithubContext = createContext();
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-export const GihubProvider = ({children}) => {
+export const GithubProvider = ({children}) => {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -14,19 +14,17 @@ export const GihubProvider = ({children}) => {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
-        })
+        });
 
         const data = await response.json();
-
+        
         setUsers(data);
         setLoading(false);
-
     }
 
     return <GithubContext.Provider value={{
         users,
-        loading,
-        fetchUsers
+        loading
     }}>
         {children}
     </GithubContext.Provider>
