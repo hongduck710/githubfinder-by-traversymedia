28252787ec59e25bc81a35fea8caada08e5ce537
hongduck710 <hongik710@gmail.com>
2024-12-02 19:52:35 +0900

    5 - user profile stats 차례

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,8 +3,6 @@ import { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
 import { useParams } from "react-router-dom";
-
-
 import GithubContext from "../context/github/GithubContext";
 
 function User(){
@@ -16,11 +14,75 @@ function User(){
         getUser(params.login);
     }, []);
 
+    const {
+        name,
+        type,
+        avatar_url,
+        location,
+        bio,
+        blog,
+        twitter_username,
+        login,
+        html_url,
+        following,
+        public_repos,
+        public_gists,
+        hireable
+    } = user
+
     if(loading) {
         return <Spinner />
     }
 
-    return (<div className="text-black">{user.login}</div>)
+    return (
+        <>
+            <div className="w-full mx-auto lg:w-10/12">
+                <div className="mb-4">
+                    <Link to="/" className="btn btn-ghost">
+                        검색으로
+                    </Link>
+
+                    <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 
+                    mb-8 md:gap-8">
+                        <div className="custom-card-image mb-6 md:mb-0">
+                            <div className="rounded-lg shadow-xl card image-full">
+                                <figure>
+                                    <img src={avatar_url} alt={login} />
+                                </figure>
+                                <div className="card-body justify-end">
+                                    <h2 className="card-title mb-0">
+                                        {name}
+                                    </h2>
+                                    <p>{login}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-2">
+                            <div className="mb-6">
+                                <h1 className="text-3xl card-title">
+                                    {name}
+                                    <div className="ml-2 mr-1 badge badge-success">
+                                        {type}
+                                    </div>
+                                    {hireable && (
+                                        <div className="mx-1 badge badge-info">
+                                            Hireable
+                                        </div>
+                                    )}
+                                </h1>
+                                <p>{bio}</p>
+                                <div className="mt-4 card-actions">
+                                    <a href={html_url} target="_blank" rel="noreferrer" className="btn btn-outline">
+                                        깃허브 프로필 방문하기
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 
 }
 
