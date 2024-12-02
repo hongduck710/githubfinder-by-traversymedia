7566c8c0559cf2cf1254eeabdf4510a6bacc0e2a
hongduck710 <hongik710@gmail.com>
2024-12-02 13:27:07 +0900

    검색 결과 초기화 까지 완료!

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -34,13 +34,17 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+    // Clear users from state
+    const clearUsers = () => dispatch({type: "CLEAR_USERS"});
+
     // Set loading
     const setLoading = () => dispatch({type: "SET_LOADING"});
 
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        searchUsers
+        searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
