e225d06fb57f061f310fc14696e5a8fa50846bb3
hongduck710 <hongik710@gmail.com>
2024-12-03 14:52:18 +0900

    2 - Move getUser to Actions File 시작

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,25 +16,7 @@ export const GithubProvider = ({children}) => {
 
     const [state,  dispatch] = useReducer(githubReducer, initialState);
 
-    // Get search results
-    const searchUsers = async (text) => {
-        setLoading();
-        const params = new URLSearchParams({
-            q: text
-        });
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        });
 
-        const { items } = await response.json();
-
-        dispatch({
-            type: "GET_USERS",
-            payload: items
-        })
-    }
 
     // Get single user
     const getUser = async (login) => {
@@ -87,11 +69,8 @@ export const GithubProvider = ({children}) => {
     const setLoading = () => dispatch({type: "SET_LOADING"});
 
     return <GithubContext.Provider value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
         getUserRepos
