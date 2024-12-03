b352173ee2ff7fb98f4336d64e84a28b79d840c2
hongduck710 <hongik710@gmail.com>
2024-12-03 14:10:37 +0900

    1 - Move searchUsers to Actions File 진행

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -14,6 +14,7 @@ function User(){
     useEffect(() => {
         getUser(params.login);
         getUserRepos(params.login);
+        // eslint-desable-next-line react-hooks/extensive-deps
     }, []);
 
     const {
